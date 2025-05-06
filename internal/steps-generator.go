bf664bb3d6979b57ef62796eb1e1752a61676619
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-05-05 23:10:26 -0300

    Implement functions to generate roadmaps

---
+++ internal/steps-generator.go
@@ -0,0 +1,63 @@
+package internal
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"prepai.app/configs"
+)
+
+type Step struct {
+	Title      string `json:"title"`
+	Type       string `json:"type"`
+	Order      int64  `json:"order"`
+	Difficulty string `json:"difficulty"`
+}
+
+type StepsResponse struct {
+	Steps []Step `json:"steps"`
+}
+
+func GenerateSteps(moduleTitle string, moduleDescription string, topics []string) (StepsResponse, error) {
+	topicsStr := strings.Join(topics, ", ")
+
+	prompt := fmt.Sprintf(`
+		Create structured, gamified steps for the following module: %v. The module description is: "%v", with this topics: %v.
+		The steps must help the candidate understand and practice for this module.
+
+			- Include between 8 and 10 steps in total.
+			- Each module should have:
+				- Title (Descriptive of the step).
+				- Type (What type of activity is: mock exam, open question, mock interview, lesson, etc).
+				- Order (To sort the steps)
+			- The last module needs to be an exam or interview to sum up this module.
+			- Sort them from easier to harder in terms of difficulty (1: easiest and X: hardesr). This number is the "order" field and add the appropiate difficulty.
+
+		Follow this JSON schema:
+		{
+			"steps": [
+				{
+					"title": string,
+					"type": string,
+					"order": int64,
+					"difficulty": string
+				}
+			]
+		}
+	`, moduleTitle, moduleDescription, topicsStr)
+
+	result, err := configs.Gemini(prompt)
+	if err != nil {
+		return StepsResponse{}, err
+	}
+
+	var steps StepsResponse
+
+	err = json.Unmarshal([]byte(result), &steps)
+	if err != nil {
+		return StepsResponse{}, err
+	}
+
+	return steps, nil
+}
