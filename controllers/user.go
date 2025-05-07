e15fbe9bfc03f0fd46467217944133bf9eb8e852
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-05-07 17:22:16 -0300

    Implement user functions

--- controllers/user.go
+++ controllers/user.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"prepai.app/models"
 	"prepai.app/utils"
 )
@@ -45,6 +47,7 @@ func Login(context *gin.Context) {
 	}
 
 	err = user.ValidateCredentials()
+
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
 		return
@@ -57,7 +60,102 @@ func Login(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusOK, gin.H{
-		"message": err,
-		"token":   token,
+		"token": token,
+	})
+}
+
+func GetUser(context *gin.Context) {
+	userIdInterface, exists := context.Get("userId")
+	if !exists {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "User ID not found in context",
+		})
+		return
+	}
+
+	userId, ok := userIdInterface.(bson.ObjectID)
+	if !ok {
+		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "Invalid user ID format",
+		})
+		return
+	}
+
+	user, err := models.GetUser(userId)
+	if err != nil {
+		fmt.Println(err)
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Failed to get user.",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "User fetched successfully.",
+		"user":    user,
+	})
+}
+
+func UpdateUser(context *gin.Context) {
+	// Logic to get and validate bsonObjectID (needs extra step)
+	userIdInterface, exists := context.Get("userId")
+	if !exists {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "User ID not found in context",
+		})
+		return
+	}
+
+	userId, ok := userIdInterface.(bson.ObjectID)
+	if !ok {
+		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "Invalid user ID format",
+		})
+		return
+	}
+
+	// Get and validate new user data
+	var updatedUser models.User
+	err := context.ShouldBindJSON(&updatedUser)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	updatedUser.Id = userId
+
+	err = updatedUser.Update()
+	if err != nil {
+		fmt.Print(err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Failed to update user."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "User updated successfully."})
+}
+
+func DeleteUser(context *gin.Context) {
+	userIdInterface, exists := context.Get("userId")
+	if !exists {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "User ID not found in context",
+		})
+		return
+	}
+
+	userId, ok := userIdInterface.(bson.ObjectID)
+	if !ok {
+		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "Invalid user ID format",
+		})
+		return
+	}
+
+	var user models.User
+	user.Id = userId
+	user.Delete()
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "User deleted successfully.",
 	})
 }
--- models/user.go
+++ models/user.go
@@ -3,6 +3,7 @@ package models
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strings"
 	"time"
 
@@ -14,14 +15,37 @@ import (
 )
 
 type User struct {
-	Id       bson.ObjectID `bson:"_id,omitempty"`
-	FullName string        `bson:"full_name,omitempty"`
-	Email    string        `bson:"email" validate:"required"`
-	Password string        `bson:"password" validate:"required"`
-	ImageUrl string        `bson:"image_url,omitempty"`
+	Id       bson.ObjectID `json:"id" bson:"_id,omitempty"`
+	FullName string        `json:"full_name" bson:"full_name,omitempty"`
+	Email    string        `json:"email" bson:"email" validate:"required,email"`
+	Password string        `json:"password" bson:"password" validate:"required"`
+	ImageUrl string        `json:"image_url" bson:"image_url,omitempty"`
 }
 
-func (user User) Save() error {
+func GetUser(userId bson.ObjectID) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	collection := configs.GetCollection("users")
+
+	projection := bson.M{
+		"password": 0,
+	}
+	opts := options.FindOne().SetProjection(projection)
+
+	var user User
+	err := collection.FindOne(ctx, bson.M{"_id": userId}, opts).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, err
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (user *User) Save() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
@@ -64,18 +88,54 @@ func (user *User) ValidateCredentials() error {
 	opts := options.FindOne().SetProjection(projection)
 
 	var result User
-	err := collection.FindOne(ctx, bson.M{"email": user.Email}, opts)
+	err := collection.FindOne(ctx, bson.M{"email": user.Email}, opts).Decode(&result)
 	if err != nil {
-		if err.Err() == mongo.ErrNoDocuments {
-			return errors.New("creadentials invalid")
+		if err == mongo.ErrNoDocuments {
+			return errors.New("credentials invalid")
 		}
-		return err.Err()
+		return err
 	}
 
 	isPasswordValid := utils.CheckPasswordHash(user.Password, result.Password)
-
 	if !isPasswordValid {
-		return errors.New("creadentials invalid")
+		return errors.New("credentials invalid")
+	}
+
+	user.Id = result.Id
+	return nil
+}
+
+func (user User) Update() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	collection := configs.GetCollection("users")
+	update := bson.M{
+		"$set": bson.M{
+			"full_name": user.FullName,
+			"image_url": user.ImageUrl,
+		},
+	}
+
+	_, err := collection.UpdateByID(ctx, user.Id, update)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (user User) Delete() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	fmt.Println(user)
+
+	collection := configs.GetCollection("users")
+
+	_, err := collection.DeleteOne(ctx, bson.M{"_id": user.Id})
+	if err != nil {
+		return err
 	}
 
 	return nil
