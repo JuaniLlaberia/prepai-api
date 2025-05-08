11e921ca38b9d34b428b11511670370f3216086e
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-05-07 21:02:54 -0300

    Add user routes and move auth routes to it's own router

--- routes/user-route.go
+++ routes/user-route.go
@@ -3,9 +3,13 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"prepai.app/controllers"
+	"prepai.app/middlewares"
 )
 
 func UserRoute(server *gin.Engine) {
-	server.POST("/signup", controllers.Signup)
-	server.POST("/login", controllers.Login)
+	authenticatedRoute := server.Group("/user")
+	authenticatedRoute.Use(middlewares.Authenticate)
+	authenticatedRoute.GET("", controllers.GetUser)
+	authenticatedRoute.PATCH("/update", controllers.UpdateUser)
+	authenticatedRoute.DELETE("/delete", controllers.DeleteUser)
 }
