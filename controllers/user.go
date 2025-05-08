a57ef2ca2e05151e30c68e26addd34a2445585de
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-05-07 21:02:03 -0300

    Move auth logic to auth controller

--- controllers/user.go
+++ controllers/user.go
@@ -7,62 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"prepai.app/models"
-	"prepai.app/utils"
 )
-
-func Signup(context *gin.Context) {
-	var user models.User
-
-	err := context.ShouldBindJSON(&user)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Could not parse request data.",
-		})
-		return
-	}
-
-	err = user.Save()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{
-			"message": err.Error(),
-		})
-		return
-	}
-
-	context.JSON(http.StatusCreated, gin.H{
-		"message": "User created successfully",
-		"data":    user,
-	})
-}
-
-func Login(context *gin.Context) {
-	var user models.User
-
-	err := context.ShouldBindJSON(&user)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Could not parse request data.",
-		})
-		return
-	}
-
-	err = user.ValidateCredentials()
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
-		return
-	}
-
-	token, err := utils.GenerateToken(user.Email, user.Id)
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user."})
-		return
-	}
-
-	context.JSON(http.StatusOK, gin.H{
-		"token": token,
-	})
-}
 
 func GetUser(context *gin.Context) {
 	userIdInterface, exists := context.Get("userId")
